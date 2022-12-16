3f6d5c789e311ee6b83c80045ff35e9570474d23
aganta12 <118507948+aganta12@users.noreply.github.com>
2022-12-16 21:24:54 +0530

    inital commit
    Adding the code, user manual, And documrntation

---
+++ main.py
@@ -0,0 +1,12 @@
+#Amar Babu Ganta
+#Kranthi Kiran Reddy Gurram
+#Syed Asad Mehdi
+#Saidi Reddy Vangala
+#project group
+
+
+from game import Game
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
